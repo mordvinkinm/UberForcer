f60495e4226482eb488a8b2b3ee51e7e21523087
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2018-07-19 19:21:28 -0500

    Added debug(format, opt.args...) macros

--- common.h
+++ common.h
@@ -5,4 +5,13 @@
 #define true 1
 #define false 0
 
+// Define debug method
+#ifdef DEBUG
+#define debug(message, ...) printf(message __VA_OPT__(,) __VA_ARGS__)
+#endif
+
+#ifndef DEBUG
+#define debug(message, ...)
+#endif
+
 #endif /* COMMON_H */
