3e6def69c79ca6b72ff52ea5ece79f06a702c401
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2018-07-20 23:34:38 -0500

    Changes in debug/verbose logging; Updated readme.md

--- common.h
+++ common.h
@@ -7,13 +7,21 @@
 #define true 1
 #define false 0
 
+#define DEBUG_VERBOSE 1
+#define DEBUG_TRACE 2
+
 // Define debug method
-#ifdef DEBUG
-#define debug(message, ...) printf(message __VA_OPT__(,) __VA_ARGS__)
+#if DEBUG >= DEBUG_LOG
+    #define debug(message, ...) printf(message __VA_OPT__(,) __VA_ARGS__)
+#else
+    #define debug(message, ...)
 #endif
 
-#ifndef DEBUG
-#define debug(message, ...)
-#endif
+#if DEBUG >= DEBUG_TRACE
+    #define trace(message, ...) printf(message __VA_OPT__(,) __VA_ARGS__)
+#else
+    #define trace(message, ...)
+#endif 
+
 
 #endif /* COMMON_H */
