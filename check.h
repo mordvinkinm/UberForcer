4ff5fa86070cb2e960c28b587fd07e536e4b7134
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2018-07-28 02:33:05 -0500

    Added documentation to check.c / check.h

--- check.h
+++ check.h
@@ -3,14 +3,71 @@
 
 #include "config.h"
 
-// Thread-unsafe versions
+/**************************************************************************
+* Function:    check_task
+*
+* Description: Thread-unsafe version of hash checker
+*
+* Inputs:      config_t *config
+*              Pointer to application config
+*
+*              task_t *result
+*              Pointer to task with generated password
+*
+* Returns:     none
+* 
+*************************************************************************/
 void check_task(config_t* config, task_t* result);
 
+
+/**************************************************************************
+* Function:    check_task_benchmark
+*
+* Description: Thread-unsafe function to calculate bruteforcing performance
+*
+* Inputs:      config_t *config
+*              Pointer to application config
+*
+*              task_t *result
+*              Pointer to task with generated password
+*
+* Returns:     none
+* 
+*************************************************************************/
 void check_task_benchmark(config_t* config, task_t* result);
 
-// Thread-safe versions
+
+/**************************************************************************
+* Function:    check_task_r
+*
+* Description: Reentrant version of hash checker
+*
+* Inputs:      config_t *config
+*              Pointer to application config
+*
+*              task_t *result
+*              Pointer to task with generated password
+*
+* Returns:     none
+* 
+*************************************************************************/
 void check_task_r(config_t* config, task_t* result);
 
+
+/**************************************************************************
+* Function:    check_task_benchmark_r
+*
+* Description: Reentrant function to calculate bruteforcing performance
+*
+* Inputs:      config_t *config
+*              Pointer to application config
+*
+*              task_t *result
+*              Pointer to task with generated password
+*
+* Returns:     none
+* 
+*************************************************************************/
 void check_task_benchmark_r(config_t* config, task_t* result);
 
 #endif /* CHECK_H */
