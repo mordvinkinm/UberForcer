39e6c46459b707cd58ece7b6600b12a4110eac6b
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2018-08-07 20:02:43 -0500

    Updated protocol - changed task name prefix (from MT to MSG), added space after %s

--- protocol.h
+++ protocol.h
@@ -1,28 +1,28 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-#define MSG_SEND_JOB               \
-  "<?xml version=\"1.0\"?>\n"      \
-  "<msg>\n"                        \
-  " <type>MT_SEND_JOB</type>\n"    \
-  " <args>\n"                      \
-  "   <job>\n"                     \
-  "     <password>%s</password>\n" \
-  "     <hash>%s</hash>\n"         \
-  "     <alphabet>%s</alphabet>\n" \
-  "     <from>%d</from>\n"         \
-  "     <to>%d</to>\n"             \
-  "   </job>\n"                    \
-  " </args>\n"                     \
+#define MSG_SEND_JOB                \
+  "<?xml version=\"1.0\"?>\n"       \
+  "<msg>\n"                         \
+  " <type>MSG_SEND_JOB</type>\n"    \
+  " <args>\n"                       \
+  "   <job>\n"                      \
+  "     <password>%s </password>\n" \
+  "     <hash>%s </hash>\n"         \
+  "     <alphabet>%s </alphabet>\n" \
+  "     <from>%d</from>\n"          \
+  "     <to>%d</to>\n"              \
+  "   </job>\n"                     \
+  " </args>\n"                      \
   "</msg>"
 
-#define MT_REPORT_RESULT                       \
+#define MSG_REPORT_RESULT                      \
   "<?xml version=\"1.0\"?>\n"                  \
   "<msg>\n"                                    \
-  " <type>MT_REPORT_RESULTS</type>\n"          \
+  " <type>MSG_REPORT_RESULTS</type>\n"         \
   " <args>\n"                                  \
   "   <result>\n"                              \
-  "     <password>%s</password>\n"             \
+  "     <password>%s </password>\n"            \
   "     <password_found>%d</password_found>\n" \
   "   </result>\n"                             \
   " </args>\n"                                 \
