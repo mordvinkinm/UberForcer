2d0dd6003becdb404758c3708544c0193fae447e
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2018-08-07 20:20:11 -0500

    Commited missed change in workers.h

--- workers.h
+++ workers.h
@@ -9,4 +9,6 @@ void single_brute(config_t *config, task_t * initial_task);
 
 void multi_brute(config_t *config, task_t * initial_task);
 
+void generate_tasks_worker(config_t *config, task_t * initial_task);
+
 #endif /* WORKERS_H */
